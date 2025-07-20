d4791436b57bfb7fe297daf939ae8dbee0e2e0a9
sajjad zeraatkar <73385188+sajjadzea@users.noreply.github.com>
2025-07-20 14:04:17 +0330

    Add RTL React app skeleton

---
+++ decision-tree-rtl-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* درخت تصمیم مشتری */}
+      <h1>درخت تصمیم مشتری</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ decision-tree-rtl-app/src/components/DecisionTree.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// TODO: پیاده‌سازی کامپوننت درخت تصمیم
+
+const DecisionTree = () => {
+  return (
+    <div>
+      {/* در اینجا درخت تصمیم نمایش داده می‌شود */}
+    </div>
+  );
+};
+
+export default DecisionTree;
