b8246ff1ae13cdd52a5731b192dc389c1f284ea9
sajjad zeraatkar <73385188+sajjadzea@users.noreply.github.com>
2025-07-21 17:20:29 +0330

    Remove duplicate header from decision tree

--- decision-tree-rtl-app/src/App.js
+++ decision-tree-rtl-app/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './index.css';
-import Header from './components/Header';
 import DecisionTree from './components/DecisionTree';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <main>
         <h1>درخت تصمیم مشتری</h1>
         <DecisionTree />
