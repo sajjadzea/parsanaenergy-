eb036d462c1cbadf61abfd8ce176a668dde2ba22
sajjad zeraatkar <73385188+sajjadzea@users.noreply.github.com>
2025-07-22 19:22:55 +0330

    Remove duplicate header

--- decision-tree-app/src/App.jsx
+++ decision-tree-app/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header.jsx';
 import Home from './pages/Home.jsx';
 import Articles from './pages/Articles.jsx';
 import Article from './pages/Article.jsx';
@@ -9,7 +8,6 @@ import Article from './pages/Article.jsx';
 export default function App() {
   return (
     <div className="App">
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/articles" element={<Articles />} />
