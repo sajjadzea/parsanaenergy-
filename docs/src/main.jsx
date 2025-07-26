7d72de25c0b59802f4f7dd3f3aafdbaeb90f9719
sajjad zeraatkar <73385188+sajjadzea@users.noreply.github.com>
2025-07-26 07:53:05 +0330

    Update docs bundle hashes

--- docs/src/main.jsx
+++ docs/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootEl = document.getElementById('root');
+if (rootEl && !rootEl._reactRootContainer) {
+  createRoot(rootEl).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
